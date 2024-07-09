dc2a8888e22a163135e5da5f99b3b06d1889c051
ScoChillOFF <cheatkiller12@gmail.com>
2024-07-09 05:02:32 +0300

    Backend: created posts table

--- backend/app/sql_app/models.py
+++ backend/app/sql_app/models.py
@@ -1,5 +1,17 @@
 import sqlalchemy.orm as so
 
+from uuid import uuid4
+from datetime import datetime, timezone
+
 
 class Base(so.DeclarativeBase):
     pass
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id: so.Mapped[str] = so.mapped_column(primary_key=True, default=lambda: str(uuid4()), index=True)
+    title: so.Mapped[str]
+    content: so.Mapped[str]
+    created_at: so.Mapped[datetime] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
