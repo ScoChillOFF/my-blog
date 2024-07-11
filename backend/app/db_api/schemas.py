1e1923478b5e1f22ea5504e95d881f78394f2190
ScoChillOFF <cheatkiller12@gmail.com>
2024-07-12 00:40:24 +0300

    Backend: refactored schemas

--- backend/app/db_api/schemas.py
+++ backend/app/db_api/schemas.py
@@ -3,22 +3,39 @@ from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
-class ArticleBase(BaseModel):
+class TagSubResponse(BaseModel):
+    id: str
+    name: str
+
+
+class TagResponse(BaseModel):
+    id: str
+    name: str
+    articles: list['ArticleResponse']
+
+
+class ArticleCreation(BaseModel):
     title: str
     content: str
 
 
-class ArticleCreation(ArticleBase):
-    pass
-
-
-class ArticleResponse(ArticleBase):
-    id:str
+class ArticleResponse(BaseModel):
+    id: str
+    title: str
+    content: str
     created_at: datetime
+    tags: list[TagSubResponse]
 
     model_config = ConfigDict(from_attributes=True)
 
 
-class ArticleUpdating(ArticleBase):
+class ArticleSubResponse(BaseModel):
+    id: str
+    title: str
+    content: str
+    created_at: str
+
+
+class ArticleUpdating(BaseModel):
     title: str | None = None
     content: str | None = None
