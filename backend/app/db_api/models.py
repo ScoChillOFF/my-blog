bd194f846bdfdcd382f0269fec38aead6910c5f1
ScoChillOFF <cheatkiller12@gmail.com>
2024-07-11 21:45:16 +0300

    Backend: created tags table

--- backend/app/db_api/models.py
+++ backend/app/db_api/models.py
@@ -16,3 +16,10 @@ class Article(Base):
     title: so.Mapped[str]
     content: so.Mapped[str]
     created_at: so.Mapped[datetime.datetime] = so.mapped_column(sa.DateTime(timezone=True), default=lambda: datetime.datetime.now(datetime.UTC))
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+
+    id: so.Mapped[str] = so.mapped_column(primary_key=True, default=lambda: str(uuid4()), index=True)
+    name: so.Mapped[str] = so.mapped_column(unique=True, index=True)
