b412be5cac170979f78fa825f046dc3621fdc2fb
ScoChillOFF <cheatkiller12@gmail.com>
2024-07-19 06:13:02 +0300

    Backend: tags creation in article creation

--- backend/my_blog/db_api/crud.py
+++ backend/my_blog/db_api/crud.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from sqlalchemy.ext.asyncio import AsyncSession
 import sqlalchemy as sa
 
@@ -16,12 +18,34 @@ async def get_tags(db_session: AsyncSession) -> list[models.Tag]:
 
 
 async def create_article(article_schema: schemas.ArticleCreation, db_session: AsyncSession) -> models.Article:
-    tags = await get_tags_by_names(db_session, article_schema.tags)
-    article = models.Article(title=article_schema.title, content=article_schema.content, tags=tags)
+    article = models.Article(title=article_schema.title, content=article_schema.content)
+    await add_tags_to_article(article, article_schema.tags, db_session)
     db_session.add(article)
     await db_session.commit()
     await db_session.refresh(article)
     return article
+
+
+async def add_tags_to_article(article: models.Article, tag_names: list[str], db_session: AsyncSession) -> None:
+    tag_names = [name.lower() for name in tag_names]
+    existing_tags = await get_tags_by_names(db_session, tag_names)
+    non_existing_tags = await create_non_existing_tags(tag_names, existing_tags, db_session)
+    all_tags = existing_tags + non_existing_tags
+    article.tags = all_tags
+
+
+async def create_non_existing_tags(
+        tag_names: list[str],
+        existing_tags: list[models.Tag],
+        db_session: AsyncSession
+) -> list[models.Tag]:
+    non_existing_tag_names = set(tag_names) - set([tag.name for tag in existing_tags])
+    tags = []
+    for name in non_existing_tag_names:
+        tag = models.Tag(name=name)
+        db_session.add(tag)
+        tags.append(tag)
+    return tags
 
 
 async def get_tags_by_names(db_session: AsyncSession, tag_names: list[str]) -> list[models.Tag]:
@@ -35,7 +59,8 @@ async def get_article(article_id: str, db_session: AsyncSession) -> models.Artic
     return article
 
 
-async def get_articles(db_session: AsyncSession, days_limit: int | None = None, tag_names: list[str] | None = None) -> list[models.Article]:
+async def get_articles(db_session: AsyncSession, days_limit: int | None = None, tag_names: list[str] | None = None) -> \
+list[models.Article]:
     query = sa.select(models.Article).order_by(models.Article.created_at.desc())
     if days_limit is not None:
         query = apply_days_limit(query, days_limit)
@@ -66,16 +91,16 @@ async def delete_article(article_id: str, db_session: AsyncSession) -> None:
     return
 
 
-async def update_article(article_id: str, article_schema: schemas.ArticleUpdating, db_session: AsyncSession) -> models.Article | None:
+async def update_article(article_id: str, article_schema: schemas.ArticleUpdating,
+                         db_session: AsyncSession) -> models.Article | None:
     article = await get_article(article_id=article_id, db_session=db_session)
     if not article:
         return
-    
+
     for attribute, value in article_schema.model_dump().items():
         if value is not None:
             setattr(article, attribute, value)
-    
+
     db_session.add(article)
     await db_session.commit()
     return article
-    
